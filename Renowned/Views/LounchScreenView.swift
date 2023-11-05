c5f79051b76ad44ac90e156d1d1b84a7e4b01fb8
Jarek <jaroslaw-kaminski@outlook.com>
2023-11-05 22:14:09 +0000

    Add Lounch screen with animation

---
+++ Renowned/Views/LounchScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  LounchScreenView.swift
+//  Renowned
+//
+//  Created by Jarek  on 05/11/2023.
+//
+
+import SwiftUI
+
+struct LounchScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LounchScreenView()
+}
