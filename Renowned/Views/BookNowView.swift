31ac40669b8397518c8d77a94877c3ff5167ab58
Jarek <jaroslaw-kaminski@outlook.com>
2023-11-07 20:26:38 +0000

    Renowned description and background edit. Book now button with new view.

---
+++ Renowned/Views/BookNowView.swift
@@ -0,0 +1,18 @@
+//
+//  BookNowView.swift
+//  Renowned
+//
+//  Created by Jarek  on 06/11/2023.
+//
+
+import SwiftUI
+
+struct BookNowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BookNowView()
+}
