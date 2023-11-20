68cf70ab503565dc49b7a29527c98f865db009e9
Jarek <jaroslaw-kaminski@outlook.com>
2023-11-20 22:26:43 +0000

    Stylist, gallery details.

---
+++ Renowned/Views/GalleryDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  GalleryDetailsView.swift
+//  Renowned
+//
+//  Created by Jarek  on 20/11/2023.
+//
+
+import SwiftUI
+
+struct GalleryDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GalleryDetailsView()
+}
---
+++ Renowned/Views/StylistDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  StylistDetailsView.swift
+//  Renowned
+//
+//  Created by Jarek  on 16/11/2023.
+//
+
+import SwiftUI
+
+struct StylistDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StylistDetailsView()
+}
